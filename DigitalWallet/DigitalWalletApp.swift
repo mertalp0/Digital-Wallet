8ac13e721bcc9bf3bec7b94c2ab7aaab0e62143c
mertalp <mertalp010@gmail.com>
2024-07-10 15:11:22 +0300

    created project

---
+++ DigitalWallet/DigitalWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalWalletApp.swift
+//  DigitalWallet
+//
+//  Created by mert alp on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct DigitalWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
